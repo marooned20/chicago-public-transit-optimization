07b424d404dfce2274e3c8037cebb1648d684304
marooned20 <th.prayag@gmail.com>
2019-12-31 21:28:35 +0100

    configure HTTP to rest-proxy

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -1,21 +1,22 @@
 """Methods pertaining to weather data"""
-from enum import IntEnum
 import json
 import logging
-from pathlib import Path
 import random
 import urllib.parse
+from enum import IntEnum
+from pathlib import Path
 
 import requests
 
-from models.producer import Producer
-
+from .producer import Producer
 
 logger = logging.getLogger(__name__)
 
 
 class Weather(Producer):
-    """Defines a simulated weather model"""
+    """
+    Defines a simulated weather model
+    """
 
     status = IntEnum(
         "status", "sunny partly_cloudy cloudy windy precipitation", start=0
@@ -30,16 +31,13 @@ class Weather(Producer):
     summer_months = set((6, 7, 8))
 
     def __init__(self, month):
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
+        topic_name = f"com.udacity.weather"
         super().__init__(
-            "weather", # TODO: Come up with a better topic name
+            topic_name,
             key_schema=Weather.key_schema,
             value_schema=Weather.value_schema,
+            num_partitions=2,  # can go into a separate config file instead of hard-coded
+            num_replicas=1  # can go into a separate config file instead of hard-coded
         )
 
         self.status = Weather.status.sunny
@@ -53,60 +51,52 @@ class Weather(Producer):
             with open(f"{Path(__file__).parents[0]}/schemas/weather_key.json") as f:
                 Weather.key_schema = json.load(f)
 
-        #
-        # TODO: Define this value schema in `schemas/weather_value.json
-        #
         if Weather.value_schema is None:
             with open(f"{Path(__file__).parents[0]}/schemas/weather_value.json") as f:
                 Weather.value_schema = json.load(f)
 
     def _set_weather(self, month):
-        """Returns the current weather"""
+        """
+        Returns the current weather
+        """
         mode = 0.0
         if month in Weather.winter_months:
             mode = -1.0
         elif month in Weather.summer_months:
             mode = 1.0
-        self.temp += min(max(-20.0, random.triangular(-10.0, 10.0, mode)), 100.0)
+        self.temp += min(max(-20.0,
+                             random.triangular(-10.0, 10.0, mode)), 100.0)
         self.status = random.choice(list(Weather.status))
 
     def run(self, month):
         self._set_weather(month)
 
-        #
-        #
-        # TODO: Complete the function by posting a weather event to REST Proxy. Make sure to
-        # specify the Avro schemas and verify that you are using the correct Content-Type header.
-        #
-        #
-        logger.info("weather kafka proxy integration incomplete - skipping")
-        #resp = requests.post(
-        #    #
-        #    #
-        #    # TODO: What URL should be POSTed to?
-        #    #
-        #    #
-        #    f"{Weather.rest_proxy_url}/TODO",
-        #    #
-        #    #
-        #    # TODO: What Headers need to bet set?
-        #    #
-        #    #
-        #    headers={"Content-Type": "TODO"},
-        #    data=json.dumps(
-        #        {
-        #            #
-        #            #
-        #            # TODO: Provide key schema, value schema, and records
-        #            #
-        #            #
-        #        }
-        #    ),
-        #)
-        #resp.raise_for_status()
+        try:
+            resp = requests.post(
+                url=f"{Weather.rest_proxy_url}/topics/{self.topic_name}",
+                headers={
+                    "Content-Type": "application/vnd.kafka.avro.v2+json"
+                },
+                data={
+                    "key_schema": json.dumps(Weather.key_schema),
+                    "value_schema": json.dumps(Weather.value_schema),
+                    "records": [
+                        {
+                            "key": {
+                                "timestamp": self.time_millis()
+                            },
+                            "value": {
+                                "temperature": self.temp,
+                                "status": self.status.name
+                            }
+                        }
+                    ]
+                }
+            )
+            resp.raise_for_status()
+        except Exception as e:
+            logger.info(f"HTTP failed with {e}")
+
 
         logger.debug(
-            "sent weather data to kafka, temp: %s, status: %s",
-            self.temp,
-            self.status.name,
-        )
+            f"sent weather data to kafka, temp: {self.temp}, status: {self.status.name}")
