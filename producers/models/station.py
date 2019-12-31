39129a238a0cbc2ccffd334a60f4f21efe4f809e
marooned20 <th.prayag@gmail.com>
2019-12-31 15:36:47 +0100

    add producer topic config

--- producers/models/station.py
+++ producers/models/station.py
@@ -7,73 +7,65 @@ from confluent_kafka import avro
 from models import Turnstile
 from models.producer import Producer
 
-
 logger = logging.getLogger(__name__)
 
 
 class Station(Producer):
-    """Defines a single station"""
-
-    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
-
-    #
-    # TODO: Define this value schema in `schemas/station_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
+    """
+    Defines a single station
+    """
+    # load AVSC for key and value
+    key_schema = avro.load(
+        f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
+    value_schema = avro.load(
+        f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
 
     def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
         self.name = name
-        station_name = (
-            self.name.lower()
-            .replace("/", "_and_")
-            .replace(" ", "_")
-            .replace("-", "_")
-            .replace("'", "")
-        )
+        station_name = (self.name.lower()
+                        .replace("/", "_and_")
+                        .replace(" ", "_")
+                        .replace("-", "_")
+                        .replace("'", "")
+                        )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
-        topic_name = f"{station_name}" # TODO: Come up with a better topic name
+        # with each unique station_name, a new topic will be created/assigned
+        topic_name = f"com.udacity.{station_name}.arrivals"
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
-            # TODO: value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Station.value_schema,
+            num_partitions=5,  # can go into a separate config file instead of hard-coded
+            num_replicas=1  # can go into a separate config file instead of hard-coded
         )
 
         self.station_id = int(station_id)
-        self.color = color
+        self.color = color  # same as line?
         self.dir_a = direction_a
         self.dir_b = direction_b
         self.a_train = None
         self.b_train = None
         self.turnstile = Turnstile(self)
 
-
     def run(self, train, direction, prev_station_id, prev_direction):
-        """Simulates train arrivals at this station"""
-        #
-        #
-        # TODO: Complete this function by producing an arrival message to Kafka
-        #
-        #
+        """
+        Simulates train arrivals at this station
+        """
+
         logger.info("arrival kafka integration incomplete - skipping")
-        #self.producer.produce(
-        #    topic=self.topic_name,
-        #    key={"timestamp": self.time_millis()},
-        #    value={
-        #        #
-        #        #
-        #        # TODO: Configure this
-        #        #
-        #        #
-        #    },
-        #)
+        self.producer.produce(
+            topic=self.topic_name,
+            key={"timestamp": self.time_millis()},
+            value={
+                "station_id": self.station_id,
+                "train_id": train.train_id,
+                "direction": direction,
+                "line": self.color,
+                "train_status": train.status,
+                "prev_station_id": prev_station_id,
+                "prev_direction": prev_direction
+            },
+        )
 
     def __str__(self):
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
@@ -89,16 +81,22 @@ class Station(Producer):
         return str(self)
 
     def arrive_a(self, train, prev_station_id, prev_direction):
-        """Denotes a train arrival at this station in the 'a' direction"""
+        """
+        Denotes a train arrival at this station in the 'a' direction
+        """
         self.a_train = train
         self.run(train, "a", prev_station_id, prev_direction)
 
     def arrive_b(self, train, prev_station_id, prev_direction):
-        """Denotes a train arrival at this station in the 'b' direction"""
+        """
+        Denotes a train arrival at this station in the 'b' direction
+        """
         self.b_train = train
         self.run(train, "b", prev_station_id, prev_direction)
 
     def close(self):
-        """Prepares the producer for exit by cleaning up the producer"""
+        """
+        Prepares the producer for exit by cleaning up the producer
+        """
         self.turnstile.close()
         super(Station, self).close()
