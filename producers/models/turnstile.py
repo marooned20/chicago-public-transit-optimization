c9af5c94e5613704a7b5523a8febdb7ae80ec7aa
marooned20 <th.prayag@gmail.com>
2019-12-31 16:08:34 +0100

    configure producer for turnstile events

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -7,19 +7,15 @@ from confluent_kafka import avro
 from models.producer import Producer
 from models.turnstile_hardware import TurnstileHardware
 
-
 logger = logging.getLogger(__name__)
 
 
 class Turnstile(Producer):
-    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
-
-    #
-    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    key_schema = avro.load(
+        f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
+    value_schema = avro.load(
+        f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
 
     def __init__(self, station):
         """Create the Turnstile"""
@@ -31,29 +27,36 @@ class Turnstile(Producer):
             .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
+        # with each unique station_name, a new topic will be created/assigned
+        topic_name = f"com.udacity.{station_name}.turnstile"
         super().__init__(
-            f"{station_name}", # TODO: Come up with a better topic name
+            topic_name,
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema,
+            num_partitions=5,  # can go into a separate config file instead of hard-coded
+            num_replicas=1  # can go into a separate config file instead of hard-coded
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
 
     def run(self, timestamp, time_step):
-        """Simulates riders entering through the turnstile."""
+        """
+        Simulates riders entering through the turnstile.
+        """
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        logger.info("turnstile kafka integration incomplete - skipping")
-        #
-        #
-        # TODO: Complete this function by emitting a message to the turnstile topic for the number
-        # of entries that were calculated
-        #
-        #
+        logger.info(f"total entries in {self.station.name}: {num_entries}")
+
+        for _ in range(num_entries):
+            
+            # TODO: change logging level later
+            logger.info(f"producing to {self.topic_name}")
+
+            self.producer.produce(
+                topic=self.topic_name,
+                key={"timestamp": self.time_millis()},
+                value={
+                    "station_id": self.station.station_id,
+                    "station_name": self.station.station_name,
+                    "line": self.color.name,
+                },
+            )
