f3a2bd3dbe19d686117990273d005a2f68672899
marooned20 <th.prayag@gmail.com>
2020-01-01 14:35:21 +0100

    create KSQL tables for turnstile and summary

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -6,36 +6,34 @@ import requests
 
 import topic_check
 
-
 logger = logging.getLogger(__name__)
 
 
 KSQL_URL = "http://localhost:8088"
 
-#
-# TODO: Complete the following KSQL statements.
-# TODO: For the first statement, create a `turnstile` table from your turnstile topic.
-#       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
-#       `turnstile` table and grouping on station_id.
-#       Make sure to cast the COUNT of station id to `count`
-#       Make sure to set the value format to JSON
-
 KSQL_STATEMENT = """
-CREATE TABLE turnstile (
-    ???
-) WITH (
-    ???
-);
+    CREATE TABLE turnstile (
+        station_id INT,
+        station_name VARCHAR,
+        line VARCHAR
+    ) WITH (
+        KAFKA_TOPIC = 'com.udacity.station.turnstile',
+        KEY = 'station_id',
+        VALUE_FORMAT = 'avro'
+    );
 
-CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+    CREATE TABLE turnstile_summary
+    WITH (value_format = 'json') AS
+        SELECT station_id, count(station_id) as num_count
+        FROM turnstile
+        GROUP BY station_id;
 """
 
 
 def execute_statement():
-    """Executes the KSQL statement against the KSQL API"""
+    """
+    Executes the KSQL statement against the KSQL API
+    """
     if topic_check.topic_exists("TURNSTILE_SUMMARY") is True:
         return
 
